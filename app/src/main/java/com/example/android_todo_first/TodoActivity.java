91552319a39274609f397700856436892106f4d5
Pravesh Pansari <praveshpansari@gmail.com>
2021-02-21 12:23:28 +0545

    initial commit

---
+++ app/src/main/java/com/example/android_todo_first/TodoActivity.java
@@ -0,0 +1,14 @@
+package com.example.android_todo_first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TodoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
